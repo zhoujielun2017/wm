d90f731a5487ed35fbc6c6eb4cc2c7103072dfc2
zhoujielun_2017 <zhoujielun_2017@163.com>
2018-04-08 22:02:40 +0800

    1

--- util/RegUtil.js
+++ util/RegUtil.js
@@ -5,13 +5,14 @@ var RegUtil={
 function replaceEmail(email_address) {
     　
     var regex = /([0-9A-Za-z\-_\.]+)@([0-9a-z]+\.[a-z]{2,3}(\.[a-z]{2})?)/g;
-    alert_string=email_address.replace(regex,'******');
+    var regex2 = /([0-9A-Za-z\-_\.]+)@([0-9a-z]+\.[0-9a-z]+\.[a-z]{2,3}(\.[a-z]{2})?)/g;
+    alert_string=email_address.replace(regex,'******').replace(regex2,'******');
     return alert_string;
 }
      
 // 替换手机字符
 function replaceMobile(str) {
-　　str=str.replace(/1[[0-9]{10}/g,'******');
+　　str=str.replace(/1[[0-9]{10}/g,'******').replace(/[1-9][[0-9]{6,7}/g,'******');
     return str;
 }　
     
