7b3c1925aa2cfabaecae74bbe8ca71b5d135a87a
zhoujielun2017 <30916042+zhoujielun2017@users.noreply.github.com>
2018-04-12 09:18:40 +0800

    Update Article.js
    add type

--- model/Article.js
+++ model/Article.js
@@ -4,5 +4,6 @@ module.exports = db.defineModel('article', {
     visit: db.BIGINT,
     title: db.STRING(100),
     tag: db.STRING(100),
+    type: db.STRING(25),
     content: db.STRING(10000)
 });
