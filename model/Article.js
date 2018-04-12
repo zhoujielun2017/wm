c6a1d76a08a36949967f8bb02fb63703bef9ae80
zhoujielun_2017 <zhoujielun_2017@163.com>
2018-04-12 21:10:11 +0800

    add type

--- model/Article.js
+++ model/Article.js
@@ -4,6 +4,6 @@ module.exports = db.defineModel('article', {
     visit: db.BIGINT,
     title: db.STRING(100),
     tag: db.STRING(100),
-    type: db.STRING(25),
+    type: db.STRING(24),
     content: db.STRING(10000)
 });
